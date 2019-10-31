789b15127de19eb3bb65a3e348bfae7fa1cd428d
Evelyne Lachance <evelyne.lachance@mindgeek.com>
2019-10-31 15:21:12 -0400

    Update deps, add ecosystem for pm2

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', function connection(ws) {
     app.context.ws = wss;
     ws.on('message', function incoming(message) {
+        console.log(message);
         wss.clients.forEach(client => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message);
